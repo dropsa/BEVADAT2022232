66bdbd7eb4e03ad4507358f403352cf82a2cbc4a
Csekity Laura <dropsadominik@gmail.com>
2023-03-06 23:28:52 +0100

    [INIT]

--- GYAK/GYAK01/GYAK01.py
+++ GYAK/GYAK01/GYAK01.py
@@ -1,9 +1,29 @@
+# %%
+#Készíts egy olyan függvényt ami paraméterként egy listát vár amiben egész számok vannak, 
+#és el kell döntenie,hogy van-e benne páratlan szám. A visszatérésí érték egy bool legyen (True:van benne,False:nincs benne)
+#Egy példa a bemenetre: [1,2,3,4,4,5]
+#Egy példa a kimenetre: True
+#return type: bool
+#függvény neve legyen: contains_odd
+
+
+# %%
 def contains_odd(input_list):
     for number in input_list:
         if number % 2 != 0:
             return True
     return False
 
+# %%
+#Készíts egy függvényt ami paraméterként egy listát vár amiben egész számok vannak,
+#és eldönti minden eleméről, hogy páratlan-e. A kimenet egy lista legyen amiben True/False értékek vannak.
+#Egy példa a bemenetre: [1,2,3,4,5]
+#Egy példa a kimenetre: [True,False,True,False,True]
+#return type: list
+#függvény neve legyen: is_odd
+
+
+# %%
 def is_odd(input_list):
     mask = []
     for number in input_list:
@@ -13,14 +33,41 @@ def is_odd(input_list):
             mask.append(True)
     return mask
 
+# %%
+#Készíts egy függvényt ami paraméterként 2 db listát vár, és kiszámolja a listák elemenként vett összegét.
+#A függvény egy listával térjen vissza amiben a megfelelő indexen lévő lista_1 és lista_2 elemek összege van.
+#Egy példa a bemenetekre: input_list_1:[1,2,3,4], input_list_2:[1,2,3,4]
+#Egy példa a kimenetre: [2,3,4,8]
+#return type: list
+#függvény neve legyen: element_wise_sum
+
+
+# %%
 def element_wise_sum(input_list_1, input_list_2):
     output_list = []
     for i in range(min(len(input_list_1), len(input_list_2))):
         output_list.append(input_list_1[i] + input_list_2[i])
     return output_list
 
+# %%
+#Készíts egy függvényt ami paraméterként egy dictionary-t vár és egy listával tér vissza
+#amiben a kulcs:érték párok egy Tuple-ben vannak.
+#Egy példa a bemenetere: {"egy":1,"ketto":2,"harom":3}
+#Egy példa a kimenetre: [("egy",1),("ketto",2),("harom",3)]
+#return type: list
+#függvény nevel egyen: dict_to_list
+
+
+# %%
 def dict_to_list(input_dict : dict) -> list:
     outputlist=[]
     for key,value in input_dict.items():
         outputlist.append((key,value))
     return outputlist
+
+# %%
+#Ha végeztél a feladatokkal akkor ezt a jupytert alakítsd át egy .py file-ra 
+#ha vscode-ban dolgozol: https://stackoverflow.com/questions/64297272/best-way-to-convert-ipynb-to-py-in-vscode
+#ha jupyter lab-ban dolgozol: https://stackoverflow.com/questions/52885901/how-to-save-python-script-as-py-file-on-jupyter-notebook
+
+
--- HAZI/HAZI01/HAZI01.py
+++ HAZI/HAZI01/HAZI01.py
@@ -1,10 +1,39 @@
+# %%
+#Készíts egy függvényt ami paraméterként egy listát vár és visszatér ennek a listának egy rész listájával.
+#Paraméterként lehessen megadni, hogy mettől-meddig akarjuk visszakapni a listát.
+#Egy példa a bemenetre: input_list=[1,2,3,4,5], start=1, end=4
+#Egy példa a kimenetre: [2,3,4]
+#NOTE: ha indexelünk és 4-et adunk meg felső határnak akkor csak a 3. indexig kapjuk vissza az értékeket a 4. már nem lesz benne
+#NOTE: és ez az elvárt viselkedés ebben a feladatban is
+#return type: list
+#függvény neve legyen: subset
 
+# %%
 def subset(input_list, start, end):
     return input_list[start:end]
 
+# %%
+#Készíts egy függvényt ami egy listát vár paraméterként és ennek a listának minden n-edik elemét adja vissza.
+#Paraméterként lehessen állítani azt hogy hanyadik elemeket szeretnénk viszakapni.
+#NOTE: a 0. elem is legyen benne
+#Egy példa a bemenetre: input_list=[1,2,3,4,5,6,7,8,9], n=3
+#Egy példa a kimenetre: [1,4,7]
+#return type: list
+#függvény neve legyen: every_nth
+
+# %%
 def every_nth(input_list, n):
     return input_list[::n]
 
+# %%
+#Készíts egy függvényt ami paraméterként egy listát vár és eldönti, hogy a listában csak egyedi értékek vannak-e
+#Egy bool-al térjen vissza: True:csak egyedi értékek vannak benne, False:van benne ismétlődés
+#Egy példa a bemenetre: [1,2,3,4,5,6,7]
+#Egy példa a kimenetre: True
+#return type: bool
+#függvény neve legyen: unique
+
+# %%
 def unique(input_list : list) -> bool:
     for i in range(0, len(input_list), 1):
         for j in range(len(input_list)-1, -1, -1):
@@ -13,6 +42,16 @@ def unique(input_list : list) -> bool:
                     return False
     return True
 
+# %%
+#Készíts egy függvényt ami paraméterként egy 2 dimenziós listát vár és ezt a listát kitudja "lapítani"
+#Egy olyan listával térjen vissza amelyben nincsen több kisebb lista, azaz egy egy dimenziós listával.
+#Egy példa a bemenetre: [[1,2],[3,4],[5,6]]
+#Egy példa a kimenetre: [1,2,3,4,5,6]
+#NOTE: csak 2 dimenziós listát kezeljen nem kell ennél mélyebbet
+#return type: list
+#függvény neve legyen: flatten
+
+# %%
 def flatten(input_list):
     output_list = []
     for sublist in input_list:
@@ -20,18 +59,47 @@ def flatten(input_list):
             output_list.append(item)
     return output_list
 
+# %%
+#Készíts egy függvényt ami paraméterként n darab listát vár, és összfűzi ezeket a listákat.
+#Egy olya listával térjen vissza ami 1 dimenziós és tartalmazza az össze bemeneti lista kértékét
+#NOTE: sorrend nem számít
+#HINT: használj *args-ot az input paraméternél
+#Egy példa a bemenetre: lista_1 = [1,2,3], lista_2 = [4,5,6], ..... lista_n = [7,8,9]
+#Egy példa a kimenetre: [1,2,3,4,5,6,7,8,9]
+#return type: list
+#függvény neve legyen: merge_lists
+
+# %%
 def merge_lists(*args):
     result = []
     for arg in args:
         result += arg
     return result
 
+# %%
+#Készíts egy függvényt ami paraméterként egy listát vár amiben 2 elemet tartalmazó tuple-ök vannak,
+#és visszatér ezeknek a tuple-nek a fordítottjával.
+#Egy példa a bemenetre: [(1,2),(3,4)]
+#Egy példa a kimenetre: [(2,1),(4,3)] 
+#return type: list
+#függvény neve legyen: reverse_tuples
+
+# %%
 def reverse_tuples(input_list):
     output_list = []
     for tpl in input_list:
         output_list.append((tpl[1], tpl[0]))
     return output_list
 
+# %%
+#Készíts egy függvényt ami paraméterként egy listát vár, és eltávolítja az ismétlődéseket a listából.
+#Egy olyan listával térjen vissza amiben csak a bemeneti lista egyedi értékei vannak benne.
+#Egy példa a bemenetre: [1,2,3,3,4,5]
+#Egy példa a kimenetre: [1,2,3,4,5]
+#return type: list
+#függvény neve legyen: remove_duplicates
+
+# %%
 def remove_tuplicates(input_list : list) -> list:
     removable=[]
     for i in range(0, len(input_list), 1):
@@ -44,6 +112,19 @@ def remove_tuplicates(input_list : list) -> list:
         input_list.remove(x)
     return input_list
 
+# %%
+#Készíts egy olyan függvényt ami paraméterként egy 2 dimenziós mátrixot vár és visszater a mátrix transzponáltjával.
+#Egy példa a bemenetre: [[1,2,3],
+#                        [4,5,6],
+#                        [7,8,9]]
+#
+#Egy példa a kimenetre: [[1,4,7],
+#                        [2,5,8],
+#                        [3,6,9]]
+#return type: list
+#függvény neve legyen: transpose
+
+# %%
 def transpose(input_list : list) -> list:
     result = []
 
@@ -59,18 +140,45 @@ def transpose(input_list : list) -> list:
     
     return result
 
+# %%
+#Készíts egy függvényt ami paraméterként egy listát vár és visszatér a lista csoportosított változatával.
+#Egy olyan listával térjen vissza amiben a paraméterként átadott chunk_size méretű listák vannak.
+#Egy példa a bemenetre: [1,2,3,4,5,6,7,8]
+#Egy példa a kimenetre: [[1,2,3],[4,5,6],[7,8]]
+#NOTE: ha nem mindegyik lista elemet lehet chunk_size méretű listába tenni akkor a maradékot a példában látott módon kezeljétek
+#return type: list
+#függvény neve legyen: split_into_chunks
+
+# %%
 def split_into_chunks(input_list, chunk_size):
     chunks = []
     for i in range(0, len(input_list), chunk_size):
         chunks.append(input_list[i:i+chunk_size])
     return chunks
 
+# %%
+#Készíts egy függvényt ami paraméterként n darab dictionary-t vár és visszatér egy darab dictionary-vel.
+#Egy olyan dict-el térjen vissza miben az n darab bemeneti dict értékei benne vannak.
+#Egy példa a bemenetre: dict_1: {"one":1,"two":2}, dict_2: {"four":4,"three":3}
+#Egy példa a kimenetre: {"one":1,"two":2,"four":4,"three":3}
+#HINT: használj *args-ot
+#függvény neve legyen: merge_dicts
+
+# %%
 def merge_dicts(*args):
     merged_dict = {}
     for arg in args:
         merged_dict.update(arg)
     return merged_dict
 
+# %%
+#Készíts egy függvényt ami paraméterként egy listát vár amiben egész számok vannak és visszatér egy dict-el amiben szét vannak szedve paritás szerint.
+#Egy példa a bemenetre: [1,2,3,4,5,6]
+#Egy példa a kimenetre: {"event":[2,4,6],"odd":[1,3,5]}
+#return type: dict
+#függvény neve legyen: by_parity
+
+# %%
 def by_parity(input_list):
     result = {'even': [], 'odd': []}
     for num in input_list:
@@ -80,10 +188,24 @@ def by_parity(input_list):
             result['odd'].append(num)
     return result
 
+# %%
+#Készíts egy függvényt ami paraméterként egy dict-et vár és visszatér egy dict-el amiben az egyes kulcsokhoz tartozó értékek átlaga van.
+#Egy példa a bemenetre:{"some_key":[1,2,3,4],"another_key":[1,2,3,4]}
+#Egy példa a kimenetre: {"some_key":2.5,"another_key":2.5}
+#return type: dict
+#függvény neve legyen: mean_key_value
+
+# %%
 def mean_key_value(d):
     result = {}
     for key in d:
         mean = sum(d[key]) / len(d[key])
         result[key] = mean
     return result
+
+# %%
+#Ha végeztél a feladatokkal akkor ezt a jupytert alakítsd át egy .py file-ra 
+#ha vscode-ban dolgozol: https://stackoverflow.com/questions/64297272/best-way-to-convert-ipynb-to-py-in-vscode
+#ha jupyter lab-ban dolgozol: https://stackoverflow.com/questions/52885901/how-to-save-python-script-as-py-file-on-jupyter-notebook
+
 
